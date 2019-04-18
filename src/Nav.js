905f74142675d21c5bc97c653d46d89893e47d74
remisimaer <remi.simaer@gmail.com>
2019-04-18 14:49:57 +0200

    first commit

---
+++ src/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Nav = () => (
+    <nav className="App-nav">
+        <li className="App-nav-item"><a>Products</a></li>
+        <li className="App-nav-item"><a>Cart</a></li>
+    </nav>
+);
+
+export default Nav;
